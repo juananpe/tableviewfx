1ea80e47c404b864d4cbff8797357053accac4b2
Juanan Pereira <juanan@ikasten.io>
2020-11-21 15:47:11 +0100

    Hyperlink support (demo)

--- src/main/java/ehu/isad/controller/ui/BozkaketaController.java
+++ src/main/java/ehu/isad/controller/ui/BozkaketaController.java
@@ -4,10 +4,17 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import ehu.isad.Main;
+import ehu.isad.model.HyperlinkCell;
+import ehu.isad.model.Item;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class BozkaketaController {
 
@@ -20,14 +27,36 @@ public class BozkaketaController {
     private URL location;
 
     @FXML
-    private TableView<?> tbData;
+    private TableView<Item> tbData;
+
+    private TableColumn<Item, String> nameColumn;
+    private TableColumn<Item, Hyperlink> urlColumn;
+
+    private ObservableList<Item> websiteList;
+
+
 
     @FXML
     private Button btnBozkaketaGorde;
 
     @FXML
     void initialize() {
+        nameColumn = new TableColumn<>("Name");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("websiteName"));
 
+        urlColumn = new TableColumn<>("Address");
+        urlColumn.setCellValueFactory(new PropertyValueFactory<>("hyperlink"));
+        urlColumn.setCellFactory(new HyperlinkCell());
+
+        tbData.getColumns().add(nameColumn);
+        tbData.getColumns().add(urlColumn);
+
+        websiteList = FXCollections.observableArrayList();
+        websiteList.add(new Item("Google", "www.google.com"));
+        websiteList.add(new Item("Facebook", "www.facebook.com"));
+        websiteList.add(new Item("Superglobals", "www.superglobals.net"));
+
+        tbData.setItems(websiteList);
     }
 
     @FXML
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ open module scenebuilder {
   requires javafx.controls;
   requires javafx.graphics;
   requires javafx.fxml;
+  requires java.desktop;
 
   exports ehu.isad;
 }
